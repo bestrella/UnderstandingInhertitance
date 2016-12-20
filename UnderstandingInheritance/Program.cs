4a584aec0375b68a2436a89b71d1c5dd3adccb3c
benjie estrella <bench323010@gmail.com>
2016-12-20 15:32:02 +0800

    Trial
    Signed-off-by: benjie estrella <bench323010@gmail.com>

---
+++ UnderstandingInheritance/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "BMW";
+            myCar.Model = "745li";
+            myCar.color = "black";
+            
+
+            printCarDetails(myCar);
+
+            Truck myTruck = new Truck();
+            myTruck.Make = "Ford";
+            myTruck.Model = "32004";
+            myTruck.color = "white";
+            myTruck.TowingCapacity = "12000";
+
+            printCarDetails(myTruck);
+
+            Console.ReadLine();
+        }
+
+        private static void printCarDetails(Car car)
+        {
+            Console.WriteLine("Here are  the car's details: {0}", car.FormatME());
+        }
+
+        class Car
+        {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string color { get; set; }
+
+
+            public string FormatME()
+            {
+                return String.Format("{0} - {1} - {2} - {3}", this.Make, 
+                    this.Model, 
+                    this.Year,
+                    this.color);
+                
+            }
+        }
+
+        class Truck : Car
+        {
+            public string  TowingCapacity { get; set; }
+        }
+
+    }
+}
